3ce312a881bc3bf492d252b0252965908e8ddf9b
nattanan borcha <nattadear.n@gmail.com>
2024-09-20 02:15:09 +0700

    first commit benn-project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar />
+    <Footer />
+    </>
+  )
+}
+
+export default App
